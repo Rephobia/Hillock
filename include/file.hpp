41121a3e3f235e4526deefde608c7f5fc267cce1
Rephobia <26533256+Rephobia@users.noreply.github.com>
2019-10-22 16:47:29 +0300

    Refactor file module
    Function read is templated

--- include/file.hpp
+++ include/file.hpp
@@ -2,18 +2,36 @@
 #define HILLOCK_FILE_HPP
 
 
-namespace model {
-	class runner;
-}
+#include <fstream>
 
-namespace view {
-	class runners;
-}
+#include <QString>
 
 namespace model::dal {
-	inline static const std::string FILE_NAME {"runners.txt"};
 	
-	view::runners* read();
+	inline static const std::string FILE_NAME {"runners.txt"};
+
+	template<typename T>
+	void read(T& storage)
+	{
+		std::ifstream file {model::dal::FILE_NAME};
+	
+		if (file.is_open()) {
+		
+			std::string line {};
+		
+			while (std::getline(file, line)) {
+
+				storage->add(QString::fromUtf8(line.c_str()));
+			
+			}
+		} else {
+			std::ofstream create_file {model::dal::FILE_NAME};
+			create_file.close();
+		}
+	
+		file.close();
+	}
+	
 	void append(const QString& filepath);
 }
 
