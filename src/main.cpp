01a7288ad966a2dd4297d17d96ab48e985640141
Linhurdos <teremdev@gmail.com>
2019-10-22 16:47:29 +0300

    Refactor file module
    Function read is templated

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,9 @@ int main(int argc, char* argv[])
 {
 	QApplication app {argc, argv};
 
-	auto runners {model::dal::read()};       
+	auto runners {new view::runners {}};
+	
+	model::dal::read(runners);
 	
 	view::mainwindow w {runners};
 	w.show();
