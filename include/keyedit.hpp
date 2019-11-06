84885fec87419b33dbb6ef102839668996a87991
Linhurdos <teremdev@gmail.com>
2019-11-06 14:03:18 +0300

    Separate view::keyedit and hotkey register

---
+++ include/keyedit.hpp
@@ -0,0 +1,23 @@
+#ifndef HILLOCK_KEYEDIT_HPP
+#define HILLOCK_KEYEDIT_HPP
+
+
+#include <QKeySequenceEdit>
+
+namespace view {
+	class keyedit;
+}
+
+
+class view::keyedit : public QKeySequenceEdit
+{
+public:
+	QKeySequence sequence();
+	void keyPressEvent(QKeyEvent* event);
+	void keyReleaseEvent(QKeyEvent* event);
+};
+
+
+
+
+#endif // HILLOCK_KEYEDIT_HPP
